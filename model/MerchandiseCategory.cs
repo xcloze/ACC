6528f83aa7381244c16dd2c4c4cb36db4db5cf9f
xcloze <38174997+xcloze@users.noreply.github.com>
2018-04-10 14:59:42 +0800

    Update MerchandiseCategory.cs

--- model/MerchandiseCategory.cs
+++ model/MerchandiseCategory.cs
@@ -6,22 +6,16 @@ using System.Text;
  
 namespace JuYuan.model
 {
-    /// <summary>
-    /// 商品类别信息
-    /// </summary>
+  
     class GoodsCategory
     {
-        /// <summary>
-        /// 商品类别编号
-        /// </summary>
+
         public String CategoryId
         {
             get;
             set;
         }
-        /// <summary>
-        /// 类别名称
-        /// </summary>
+
         public String CategoryName
         {
             get;
