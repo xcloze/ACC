c7cc1ef746f8a8d955e41066344dd0810927fdcd
xcloze <38174997+xcloze@users.noreply.github.com>
2018-04-10 14:49:56 +0800

    Update ConsumeInfo.cs

--- model/ConsumeInfo.cs
+++ model/ConsumeInfo.cs
@@ -5,42 +5,34 @@ using System.Text;
 
 namespace JuYuan.model
 {
-    /// <summary>
-    /// 消费信息实体
-    /// </summary>
+  
     public class ConsumeInfo
     {
-        public string order_id { get; set; }  //消费单号
+        public string order_id { get; set; }  
 
-        public string member_id { get; set; } //会员id
+        public string member_id { get; set; }
         /// <summary>
         /// 
         /// </summary>
-        public decimal consume_value { get; set; }   //消费金额
+        public decimal consume_value { get; set; }   
        
-        public DateTime dt { get; set; }  // 消费日期
+        public DateTime dt { get; set; }  
         
-        public decimal discounted_value { get; set; }   // 打折后金额
+        public decimal discounted_value { get; set; }  
 
-        //所得积分
+
         public int score { get; set; }   
 
-        public decimal pledge_amount { get; set; }    //押金总金额
+        public decimal pledge_amount { get; set; }    
+        public decimal total_amount { get; set; }     
       
-        public decimal total_amount { get; set; }     // 合计总金额
-      
-        public string comment { get; set; }  //备注
-      
-        public string optr { get; set; }     // 操作员
-      
-        public decimal from_value_card { get; set; }      //储值卡
+        public string comment { get; set; } 
+    
+        public decimal from_value_card { get; set; }    
 
+        public int pay_mode { get; set; } 
 
-        public int pay_mode { get; set; } //   支付方式,0-现金,1-微信,2-支付宝,4-银行卡,8-红码  9-代金卷
-
-
-        public decimal pay_value { get; set; }  //支付金额
-      
+        public decimal pay_value { get; set; } 
        
     }
 }
