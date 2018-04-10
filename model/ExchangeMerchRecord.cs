92a06fc065ef80363224b58e2cbb45272906d579
xcloze <38174997+xcloze@users.noreply.github.com>
2018-04-10 14:56:32 +0800

    Update ExchangeMerchRecord.cs

--- model/ExchangeMerchRecord.cs
+++ model/ExchangeMerchRecord.cs
@@ -5,45 +5,27 @@ using System.Text;
 
 namespace JuYuan.model
 {
-    /// <summary>
-    /// 兑换商品记录
-    /// </summary>
+   
     public class ExchangeGoodsRecord
     {
-        /// <summary>
-        /// 兑换商品ID
-        /// </summary>
+       
         public string UUID { get; set; }
-        /// <summary>
-        /// 会员卡号
-        /// </summary>
+       
         public string CardID { get; set; }
 
         public string MemberID { get; set; }
 
-        /// <summary>
-        /// 商品编号
-        /// </summary>
+       
         public string GoodsID { get; set; }
-        /// <summary>
-        /// 兑换日期
-        /// </summary>
+       
         public DateTime Date { get; set; }
-        /// <summary>
-        /// 兑换数量
-        /// </summary>
+      
         public int Num { get; set; }
-        /// <summary>
-        /// 兑换积分
-        /// </summary>
+       
         public int ExchangeScore { get; set; }
-        /// <summary>
-        /// 剩余积分
-        /// </summary>
+      
         public int RemainScore { get; set; }
-        /// <summary>
-        /// 管理员
-        /// </summary>
+      
         public string Opter { get; set; }
     }
 }
