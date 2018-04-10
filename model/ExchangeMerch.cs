5680199cbe0a8072290343b53d15749ab811a78f
xcloze <38174997+xcloze@users.noreply.github.com>
2018-04-10 14:55:50 +0800

    Update ExchangeMerch.cs

--- model/ExchangeMerch.cs
+++ model/ExchangeMerch.cs
@@ -5,31 +5,23 @@ using System.Text;
 
 namespace JuYuan.model
 {
-    /// <summary>
-    /// 兑换商品信息
-    /// </summary>
+   
     class ExchangeGoods
     {
-        /// <summary>
-        /// 商品编号
-        /// </summary>
+     
         public String GoodsID
         {
             get;
             set;
         }
         
-        /// <summary>
-        /// 商品数量
-        /// </summary>
+       
         public int Num
         {
             get;
             set;
         }
-        /// <summary>
-        /// 兑换积分
-        /// </summary>
+      
         public int ExchangeScore
         {
             get;
