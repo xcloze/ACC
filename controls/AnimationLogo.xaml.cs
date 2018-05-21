4ca8c42a1ed7586614f2c31c793bec87e5c23c6f
xcloze <38174997+xcloze@users.noreply.github.com>
2018-05-21 13:48:30 +0800

    Update AnimationLogo.xaml.cs

--- controls/AnimationLogo.xaml.cs
+++ controls/AnimationLogo.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace JuYuan.controls
 {
